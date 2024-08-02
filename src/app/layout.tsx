8c78de4cd5fec73ea8f56f0f3124f2f7e0192a0b
Dian Ananda <dionananda77@gmail.com>
2024-08-02 21:01:11 +0700

    update eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import "./globals.css";
-import cn from "@/lib/helper";
-import { Navbar } from "@/components/nav";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+import cn from '@/lib/helper';
+
+import { Navbar } from '@/components/nav';
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://dianananda.vercel.app"),
+  metadataBase: new URL('https://dianananda.vercel.app'),
   title: {
-    default: "Dian Ananda",
-    template: "%s | Dian Ananda",
+    default: 'Dian Ananda',
+    template: '%s | Dian Ananda',
   },
-  description: "Developer from indonesian",
+  description: 'Developer from indonesian',
   openGraph: {
-    title: "Dian Ananda",
-    description: "Developer from indonesian",
-    url: "https://dianananda.vercel.app",
-    siteName: "Dian Ananda",
-    locale: "id-ID",
-    type: "website",
+    title: 'Dian Ananda',
+    description: 'Developer from indonesian',
+    url: 'https://dianananda.vercel.app',
+    siteName: 'Dian Ananda',
+    locale: 'id-ID',
+    type: 'website',
   },
   robots: {
     index: true,
@@ -28,14 +31,14 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
     },
   },
   twitter: {
-    title: "Dian Ananda",
-    card: "summary_large_image",
+    title: 'Dian Ananda',
+    card: 'summary_large_image',
   },
 };
 
@@ -46,15 +49,15 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang='en'
       className={cn(
-        "text-black bg-white dark:text-white dark:bg-[#111010]",
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
         GeistSans.variable,
-        GeistMono.variable
+        GeistMono.variable,
       )}
     >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
+        <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
           <Navbar />
           {children}
           <Analytics />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+      <h1 className='font-medium text-2xl mb-8 tracking-tighter'>
         Hello, Next.js!
       </h1>
     </section>
