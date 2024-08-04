c56e857441e3fc87b4cb6a36c7f51c46d6bb9da8
Dian Ananda <dionananda77@gmail.com>
2024-08-04 19:03:35 +0700

    fix: update nextconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,32 @@
-import type { Config } from "tailwindcss";
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/content/**/*.mdx',
+    './public/**/*.svg',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
       },
       typography: {
         quoteless: {
           css: {
-            "blockquote p:first-of-type::before": { content: "none" },
-            "blockquote p:first-of-type::after": { content: "none" },
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
           },
         },
       },
     },
   },
-  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [typography],
 };
 export default config;
